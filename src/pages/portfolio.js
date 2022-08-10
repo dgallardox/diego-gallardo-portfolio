ed2e5afbace62de45d1b5f8be11ecd9b86c510a1
Diego Gallardo <diego.gallardo@wpengine.com>
2022-08-10 12:02:21 -0500

    improvements

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,43 +1,42 @@
+import { useState } from "react";
 
-export default function Portfolio(posts) {
-  console.log(posts);
+export default function Portfolio() {
+  const [data, setData] = useState("");
+
+  function handleSelection(e) {
+    console.log(e.target.innerText);
+    setData(e.target.innerText);
+  }
 
   return (
     <>
       <div className="text-center pt-20">
         <div className="text-3xl font-bold">Portfolio</div>
         <ul className="flex-1 flex mr-auto justify-around">
-          <li className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid">
+          <li
+            onClick={handleSelection}
+            className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid"
+          >
             All
           </li>
-          <li className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid">
+          <li
+            onClick={handleSelection}
+            className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid"
+          >
             Engineering
           </li>
-          <li className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid">
+          <li
+            onClick={handleSelection}
+            className="hover:text-blue-800 hover:underline hover:underline-offset-4 decoration-solid"
+          >
             Design
           </li>
         </ul>
+      </div>
+      <div>
+        <h4>{data}</h4>
       </div>
     </>
   );
 }
 
-// export async function getStaticProps() {
-//   const  data  = await client.query({
-//       query: gql`
-//         query getPostTitles {
-//           posts {
-//             nodes {
-//               title
-//             }
-//           }
-//         }
-//         `,
-//   });
-
-
-//   return {
-//     props: {
-//       posts: data.posts.nodes,
-//   }};
-// }
