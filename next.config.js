28e77871313c990ed2607432f28904331f63da5b
Diego Gallardo <101141475+dgallardox@users.noreply.github.com>
2022-08-18 16:06:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,15 +6,6 @@ const nextConfig = {
   },
   reactStrictMode: false,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/contact',
-        destination: 'https://www.google.com',
-        permanent: false,
-      },
-    ]
-  }
 };
 
 module.exports = nextConfig
